57cdeba2258d11fa76db86b5102841235248e298
梁 <1227689971@qq.com>
2021-08-19 00:04:31 +0800

    1uu

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Use https://hub.docker.com/r/tiangolo/uwsgi-nginx-flask/
 # hello world demo
 FROM tiangolo/uwsgi-nginx-flask:python3.8
-
+RUN
 COPY ./app /app
--- app/main.py
+++ app/main.py
@@ -6,13 +6,8 @@ app = Flask(__name__)
 
 @app.route('/<id>')
 def hello_world(id):
-    try:
-        import contentTest1
-        return contentTest1.main(id)
+    return 100+2
 
-    except Exception as e:
-        pass
-    return 'OH NO!\n'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
